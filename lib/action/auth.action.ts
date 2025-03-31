ba7211d94e93d87a5297dea81a046c4908bcdc20
aryan-prog <mandelakalam12364@gmail.com>
2025-03-31 15:26:40 +0530

    implement auth and storage

--- lib/action/auth.action.ts
+++ lib/action/auth.action.ts
@@ -25,7 +25,7 @@ export async function signUp(params: SignUpParams) {
             success: true,
             message: 'Account created successfully. Please sign in.'
         }
-        
+
     } catch (e: any) {
         console.error('Error creating a user', e);
 
@@ -81,4 +81,38 @@ export async function setSessionCookie(idToken: string) {
         path: '/',
         sameSite: 'lax'
     })
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+    const cookieStore = await cookies();
+
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if(!sessionCookie) return null;
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+        const userRecord = await db.
+            collection('users')
+            .doc(decodedClaims.uid)
+            .get();
+
+        if(!userRecord.exists) return null;
+
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User;
+    } catch (e) {
+        console.log(e)
+
+        return null;
+    }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentUser();
+
+    return !!user; //existence or non existence of user will turn into a boolean value
 }
